36c0a5e6a5a8be8f7441d0d0df368539d8ad9513
Esakkiraja <esakkiraja.p@rakuten.com>
2021-10-01 16:50:33 +0530

    Updated coverage title

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -87,7 +87,7 @@ module Danger
       puts "average_coverage_target_title: #{average_coverage_target_title}"
       if display_only_average_coverage && average_coverage_target_title.length > 0
         puts "INSIDE IF CONDITAION"
-        report_markdown = "Current coverage for RCS is 0.00%"  #"Current coverage for #{average_coverage_target_title} is #{(report.coverage * 100)}%"
+        report_markdown = "Current coverage for #{average_coverage_target_title} is #{(report.coverage * 100)}%"
       else
         puts "INSIDE ELSE CONDITAION"
         report_markdown = report.markdown_value
