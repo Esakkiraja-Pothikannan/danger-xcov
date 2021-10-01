92235a8f1c329ee968d5bdbd6fef94cfcaf6a462
Esakkiraja <esakkiraja.p@rakuten.com>
2021-10-01 14:23:41 +0530

    Added dummy string to check syntex

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -78,11 +78,12 @@ module Danger
     # Outputs a processed report with Danger
     def output_report(report, *args)
 
+      report.print_description
       # Create markdown
       display_only_average_coverage = args.first[:display_only_average_coverage] || false
       average_coverage_target_title = args.first[:average_coverage_target_title] || ""
       if display_only_average_coverage && average_coverage_target_title.count > 0
-        report_markdown = "Current coverage for #{average_coverage_target_title} is #{(report.coverage * 100)}%"
+        report_markdown = "Current coverage for RCS is 0.00%"  #"Current coverage for #{average_coverage_target_title} is #{(report.coverage * 100)}%"
       else
         report_markdown = report.markdown_value
       end
